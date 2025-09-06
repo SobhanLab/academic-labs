0c99839d4e480e242073e0695be8ad6df1a37fdf
Abdus Sobhan <abdus.sobhan.vu@gmail.com>
2025-09-06 22:34:08 +0600

    Auto update

--- CSE 2206 - Numerical Methods Lab/Lab 3/False position method.py
+++ CSE 2206 - Numerical Methods Lab/Lab 3/False position method.py
@@ -1,40 +1,41 @@
 def f(x):
     return 0.5*x*x*x - x*x
 
-def FalsePosition(f, xl, xu, max_itr=300, eps=0.05):
-    if f(xl) * f(xu) > 0:
+def FalsePosition(f, xl, xu, max_itr=500, eps=0.00):
+
+    if f(xl)*f(xu) > 0:
         print("Wrong guess!")
         return None
     
-    if f(xl) * f(xu) == 0:
+    if f(xl)*f(xu) == 0:
         if f(xl) == 0:
             return xl
         else:
             return xu
-
+        
     itr = 1
-    xr_old = xl
+    xr_old = (xl*f(xu) - xu*f(xl)) / (f(xu) - f(xl))
+
     while True:
-        xr_new = xu - (f(xu) * (xl - xu)) / (f(xl) - f(xu))
+        if f(xl) * f(xr_old) > 0:
+            xl = xr_old
+        elif f(xl)* f(xr_old) < 0:
+            xu = xr_old
+        else:
+            return xr_old
+        
+        xr_new = (xl*f(xu) - xu*f(xl)) / (f(xu) - f(xl))
         ae = abs(xr_new - xr_old)
         xr_old = xr_new
         
-        if f(xl) * f(xr_new) < 0:
-            xu = xr_new
-        elif f(xl) * f(xr_new) > 0:
-            xl = xr_new
-        else:
-            return xr_new
-        
-        itr += 1
+        itr = itr+1
         if ae <= eps or itr > max_itr:
             break
-
     return xr_old
 
 xl, xu = 1, 3
 xr = FalsePosition(f, xl, xu)
 
 if xr is not None:
-    print(f"The root of this function: {xr:.3f}")
-    print(f"The value of the function at xr: f(xr) = {f(xr):.3f}")
+    print(f"The root of this function: {xr: .3f}")
+    print(f"The value of the function is f(xr): {abs(f(xr)): .3f}")
